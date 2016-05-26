202cde4d9ca1676f75580cf7bbfd0b52121164f0
Take Mouri <takehiromouri@gmail.com>
2016-05-26 23:05:15 +0900

    remove duplicate agent

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   def login_airbnb
-    agent = Mechanize.new
-    agent.user_agent_alias = 'Mac Safari'
-    agent.follow_meta_refresh = true
+    @agent = Mechanize.new
+    @agent.user_@agent_alias = 'Mac Safari'
+    @agent.follow_meta_refresh = true
 
-    page = agent.get('https://www.airbnb.jp/')
-    page = agent.page.link_with(:href => '/login').click
+    page = @agent.get('https://www.airbnb.jp/')
+    page = @agent.page.link_with(:href => '/login').click
     form = page.forms[1]
     form.action = "/authenticate"
     form.email = airbnb_email
     form.password = airbnb_password
     page = form.submit
 
-    @agent = agent
   end
 
   def messages
