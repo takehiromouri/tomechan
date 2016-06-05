7010604f6628249673aecb6ab037de65b0ba75dc
Take Mouri <takehiromouri@gmail.com>
2016-06-06 00:17:45 +0900

    precompile assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
 
   get 'static_pages/home', as: 'home'
   get 'presignup', to: 'static_pages#presignup', as: 'presignup'
-  get 'dashboard/index', as: 'dashboard'
+  # get 'dashboard/index', as: 'dashboard'
 
 end
